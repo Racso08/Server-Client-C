ff437ee0785a291df4e7919e366dacea4132221d
Racso08 <oscar080899@gmail.com>
2019-06-10 19:11:38 -0600

    Server and Client

---
+++ Cliente/cmake-build-debug/CMakeFiles/Cliente.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cliente.dir/main.c.o"
+  "Cliente.pdb"
+  "Cliente"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Cliente.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Progra3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Progra3.dir/main.c.o"
+  "Progra3.pdb"
+  "Progra3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Progra3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
